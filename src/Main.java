c75930242b460225e252a454036357a398fc0593
Henry García <henrygarciaospina@gmail.com>
2025-10-13 16:57:23 -0500

    Definiendo métodos de las películas.

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,20 @@ public class Main {
     public static void main(String[] args) {
 
        Pelicula miPelicula1 = new Pelicula();
-
        miPelicula1.nombre = "Encanto";
        miPelicula1.fechaDeLanzamiento = 2021;
        miPelicula1.duracionEnMinutos = 120;
+       miPelicula1.mostrarFichaTecnica();
+       miPelicula1.evaluar(7.80);
+       miPelicula1.evaluar(10.00);
+       System.out.println(miPelicula1.totalEvaluaciones);
+       System.out.println(miPelicula1.sumaEvaluaciones);
+       System.out.println(miPelicula1.calcularMediaEvaluaciones());
 
-        System.out.println("Mi pelicula es: " + miPelicula1.nombre);
-        System.out.println("Fecha de lanzamiento: " + miPelicula1.fechaDeLanzamiento);
-        System.out.println("Duración en minutos: " + miPelicula1.duracionEnMinutos);
-
-        Pelicula miPelicula2 = new Pelicula();
-
-        miPelicula2.nombre = "Matrix";
-        miPelicula2.fechaDeLanzamiento = 1998;
-        miPelicula2.duracionEnMinutos = 180;
-
-        System.out.println("Mi pelicula es: " + miPelicula2.nombre);
-        System.out.println("Fecha de lanzamiento: " + miPelicula2.fechaDeLanzamiento);
-        System.out.println("Duración en minutos: " + miPelicula2.duracionEnMinutos);
+       Pelicula miPelicula2 = new Pelicula();
+       miPelicula2.nombre = "Matrix";
+       miPelicula2.fechaDeLanzamiento = 1998;
+       miPelicula2.duracionEnMinutos = 180;
+       miPelicula2.mostrarFichaTecnica();
     }
 }
--- src/Pelicula.java
+++ src/Pelicula.java
@@ -3,5 +3,22 @@ public class Pelicula {
     int fechaDeLanzamiento;
     int duracionEnMinutos;
     boolean incluidoEnElPlan;
+    double sumaEvaluaciones;
 
+    int totalEvaluaciones;
+
+    void mostrarFichaTecnica() {
+        System.out.println("Nombre de la película: " + nombre);
+        System.out.println("Fecha de lanzamiento: " + fechaDeLanzamiento);
+        System.out.println("Duración en minutos: " + duracionEnMinutos);
+    }
+
+    void evaluar(double nota) {
+        totalEvaluaciones++;
+        sumaEvaluaciones += nota;
+    }
+
+    double calcularMediaEvaluaciones() {
+        return sumaEvaluaciones / totalEvaluaciones;
+    }
 }
