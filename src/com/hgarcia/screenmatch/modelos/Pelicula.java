79352f30f3a9c037f047e5a19beab66e36d46ba7
Henry García <henrygarciaospina@gmail.com>
2025-10-18 14:54:15 -0500

    Aprovechando el modelo para serires.

--- src/com/hgarcia/screenmatch/modelos/Pelicula.java
+++ src/com/hgarcia/screenmatch/modelos/Pelicula.java
@@ -1,73 +1,13 @@
 package com.hgarcia.screenmatch.modelos;
 
-public class Pelicula {
-    private String nombre;
-    private int fechaDeLanzamiento;
-    private int duracionEnMinutos;
-    private boolean incluidoEnElPlan;
-    private double sumaEvaluaciones;
-    private int totalEvaluaciones;
+public class Pelicula extends Titulo {
+    private String director;
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public String getDirector() {
+        return director;
     }
 
-    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
-        this.fechaDeLanzamiento = fechaDeLanzamiento;
-    }
-
-    public void setDuracionEnMinutos(int duracionEnMinutos) {
-        this.duracionEnMinutos = duracionEnMinutos;
-    }
-
-    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
-        this.incluidoEnElPlan = incluidoEnElPlan;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public int getFechaDeLanzamiento() {
-        return fechaDeLanzamiento;
-    }
-
-    public int getDuracionEnMinutos() {
-        return duracionEnMinutos;
-    }
-
-    public boolean isIncluidoEnElPlan() {
-        return incluidoEnElPlan;
-    }
-
-    public double getSumaEvaluaciones() {
-        return sumaEvaluaciones;
-    }
-
-    public void setSumaEvaluaciones(double sumaEvaluaciones) {
-        this.sumaEvaluaciones = sumaEvaluaciones;
-    }
-
-    public int getTotalEvaluaciones() {
-        return totalEvaluaciones;
-    }
-
-    public void setTotalEvaluaciones(int totalEvaluaciones) {
-        this.totalEvaluaciones = totalEvaluaciones;
-    }
-
-    public void mostrarFichaTecnica() {
-        System.out.println("Nombre de la película: " + nombre);
-        System.out.println("Fecha de lanzamiento: " + fechaDeLanzamiento);
-        System.out.println("Duración en minutos: " + duracionEnMinutos);
-    }
-
-    public void evaluar(double nota) {
-        totalEvaluaciones++;
-        sumaEvaluaciones += nota;
-    }
-
-    public double calcularMediaEvaluaciones() {
-        return sumaEvaluaciones / totalEvaluaciones;
+    public void setDirector(String director) {
+        this.director = director;
     }
 }
---
+++ src/com/hgarcia/screenmatch/modelos/Titulo.java
@@ -0,0 +1,73 @@
+package com.hgarcia.screenmatch.modelos;
+
+public class Titulo {
+    private String nombre;
+    private int fechaDeLanzamiento;
+    private int duracionEnMinutos;
+    private boolean incluidoEnElPlan;
+    private double sumaEvaluaciones;
+    private int totalEvaluaciones;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
+        this.incluidoEnElPlan = incluidoEnElPlan;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
+    }
+
+    public int getDuracionEnMinutos() {
+        return duracionEnMinutos;
+    }
+
+    public boolean isIncluidoEnElPlan() {
+        return incluidoEnElPlan;
+    }
+
+    public double getSumaEvaluaciones() {
+        return sumaEvaluaciones;
+    }
+
+    public void setSumaEvaluaciones(double sumaEvaluaciones) {
+        this.sumaEvaluaciones = sumaEvaluaciones;
+    }
+
+    public int getTotalEvaluaciones() {
+        return totalEvaluaciones;
+    }
+
+    public void setTotalEvaluaciones(int totalEvaluaciones) {
+        this.totalEvaluaciones = totalEvaluaciones;
+    }
+
+    public void mostrarFichaTecnica() {
+        System.out.println("Nombre de la película: " + nombre);
+        System.out.println("Fecha de lanzamiento: " + fechaDeLanzamiento);
+        System.out.println("Duración en minutos: " + duracionEnMinutos);
+    }
+
+    public void evaluar(double nota) {
+        totalEvaluaciones++;
+        sumaEvaluaciones += nota;
+    }
+
+    public double calcularMediaEvaluaciones() {
+        return sumaEvaluaciones / totalEvaluaciones;
+    }
+}
